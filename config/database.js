bf27f4908859efbca05d2fb8432f2f7d606d27a1
vellovaherpuu <vello@bitweb.ee>
2015-12-21 10:44:47 +0200

    mongo url change

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ module.exports = {
 
 	// the database url to connect
 	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-    url: "mongodb://heroku_dxws30t3:4lf3m13c8e4a3vgttfv2hn4cbv@ds033255.mongolab.com:33255/heroku_dxws30t3"
+    url: "mongodb://heroku_sfqs2wfl:97psaeosidnad26oemn5dhh0cg@ds033285.mongolab.com:33285/heroku_sfqs2wfl"
 }
