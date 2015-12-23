60bc185dd1b213709428c15e1af40329eb504505
vellovaherpuu <vello@bitweb.ee>
2015-12-23 12:12:43 +0200

    Enviromental variables + local fallback

--- app/modules/config.js
+++ app/modules/config.js
@@ -1,4 +1,4 @@
 /**
  * Created by vellovaherpuu on 22/12/15.
  */
-exports.secretToken = "aMdoeb5ed87zorRdkD6greDML81DcnrzeSD648ferFejmplx";
+exports.secretToken = process.env.MONGOLAB_URI || "aMdoeb5ed87zorRdkD6greDML81DcnrzeSD648ferFejmplx";
--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ module.exports = {
 
 	// the database url to connect
 	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-    url: "mongodb://heroku_sfqs2wfl:97psaeosidnad26oemn5dhh0cg@ds033285.mongolab.com:33285/heroku_sfqs2wfl"
+    url: process.env.MONGOLAB_URI || "mongodb://heroku_sfqs2wfl:97psaeosidnad26oemn5dhh0cg@ds033285.mongolab.com:33285/heroku_sfqs2wfl"
 }
