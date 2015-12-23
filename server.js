c547851138189212ec45ccade39f5ed6afbd83d0
vellovaherpuu <vello@bitweb.ee>
2015-12-23 12:01:22 +0200

    testing env vars

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var bcrypt = require('bcrypt');
+
+console.log(process.env.MONGOLAB_URI);
 // configuration ===============================================================
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
