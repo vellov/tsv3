3e15092db35a71ba9b59ba91984f23016b109b52
vellovaherpuu <vello@bitweb.ee>
2016-03-20 12:26:16 +0200

    json size limit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
-//app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ limit: '50mb' })); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' , limit: '50mb' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
