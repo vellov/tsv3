93392aecd16bbf02bbe9388e3475ba4ab3b47d4c
Vello Vaherpuu <vello@bitweb.ee>
2016-05-10 23:24:25 +0300

    Removed duplicate ckeditor

--- env.js
+++ env.js
@@ -1,4 +1,7 @@
 /**
  * Created by vellovaherpuu on 06/01/16.
  */
-process.env['MONGOLAB_URI'] = "yolki";
+process.env['MONGOLAB_URI'] = "DATABASE URI";
+process.env['SECRET_TOKEN'] = "abcsad sadasdas dasdasdsadsad";
+process.env['SENDGRID_USER'] = "SENDGRID USER";
+process.env['SENDGRID_PASSWORD'] = "SENDGRID PASSWORD";
